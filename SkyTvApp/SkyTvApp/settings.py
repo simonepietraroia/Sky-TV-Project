367a17dccc74599cd48607cc8811e9f85e5a9a8e
simonepietraroia <simonepietraroia21@gmail.com>
2025-03-14 14:02:48 +0000

    added logo to nav bar. need to resize

--- SkyTvApp/SkyTvApp/settings.py
+++ SkyTvApp/SkyTvApp/settings.py
@@ -1,3 +1,4 @@
+import os
 """
 Django settings for SkyTvApp project.
 
@@ -116,7 +117,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'DevSign_Vote/static'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
