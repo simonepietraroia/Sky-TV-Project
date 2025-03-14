671a87cf75878b72a84e19fe708fff3bbf01a4d6
simonepietraroia <simonepietraroia21@gmail.com>
2025-03-14 18:17:38 +0000

    created user and healthchecksession models and migrated

--- SkyTvApp/DevSign_Vote/models.py
+++ SkyTvApp/DevSign_Vote/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, Group, Permission
 
-# Create your models here.
+class User(AbstractUser):
+    ROLE_CHOICES = [
+        ('engineer', 'Engineer'),
+        ('team_leader', 'Team Leader'),
+        ('department_leader', 'Department Leader'),
+        ('senior_manager', 'Senior Manager'),
+    ]
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='engineer')
+
+    groups = models.ManyToManyField(Group, related_name="custom_user_groups", blank=True)
+    user_permissions = models.ManyToManyField(Permission, related_name="custom_user_permissions", blank=True)
+
+    def is_team_leader(self):
+        return self.role == 'team_leader'
+
+class HealthCheckSession(models.Model):
+    name = models.CharField(max_length=100, unique=True, primary_key=True)  
+    start_date = models.DateField()
+    end_date = models.DateField()
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
--- SkyTvApp/SkyTvApp/settings.py
+++ SkyTvApp/SkyTvApp/settings.py
@@ -126,3 +126,6 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+AUTH_USER_MODEL = 'DevSign_Vote.User'
