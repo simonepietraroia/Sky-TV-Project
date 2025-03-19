a03d487ba12738c623c4ba465859fdcf381498e9
simonepietraroia <simonepietraroia21@gmail.com>
2025-03-19 21:25:37 +0000

    created registration form on forms.py

--- SkyTvApp/DevSign_Vote/forms.py
+++ SkyTvApp/DevSign_Vote/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
 
 class ProfileUpdateForm(forms.ModelForm):
 
@@ -17,3 +18,10 @@ class ProfileUpdateForm(forms.ModelForm):
         if commit:
             user.save()
         return user
+    
+class UserRegisterForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+
+    class Meta:
+        model = User
+        fields = ["username", "email", "password1", "password2"]
--- SkyTvApp/DevSign_Vote/views.py
+++ SkyTvApp/DevSign_Vote/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from .forms import ProfileUpdateForm
+from .forms import UserRegisterForm
 # from .models import Session, Vote, Team, Department, AggregateVotesTable, TrendAnalysis
 
 
