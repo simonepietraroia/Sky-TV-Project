0e918aaacde1e47ad7e6de2e6d18bcc10238fa9b
Benyamin Mahamed <benyaminmahamed@gmail.com>
2025-03-24 11:37:15 +0000

    added dropdown options and JS to support charts

--- SkyTvApp/SkyTvApp/settings.py
+++ SkyTvApp/SkyTvApp/settings.py
@@ -130,7 +130,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'DevSign_Vote.User'
 
 CSRF_TRUSTED_ORIGINS = [
-    'https://8000-simonepietr-skytvprojec-qkluzy3c9i4.ws-eu118.gitpod.io'
+    'https://8000-simonepietr-skytvprojec-qkluzy3c9i4.ws-eu118.gitpod.io', 
+    'https://8000-simonepietr-skytvprojec-16jclrgb4pw.ws-eu118.gitpod.io'
 ]
 
 AUTHENTICATION_BACKENDS = [
