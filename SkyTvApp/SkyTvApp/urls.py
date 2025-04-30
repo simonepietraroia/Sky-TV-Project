241209ffb40fa4fd3f199b8c267db189860707b9
Benyamin Mahamed <benyaminmahamed@gmail.com>
2025-04-30 20:31:05 +0000

    final fixes

--- SkyTvApp/SkyTvApp/urls.py
+++ SkyTvApp/SkyTvApp/urls.py
@@ -29,7 +29,7 @@ urlpatterns = [
     path('vote/<int:session_id>/', vote_on_session, name='voting'),
     path('sessions/', session_select, name='session-select'),
     path('sessions/<int:session_id>/', join_session, name='join_session'),
-    path('sessions/<int:session_id>/portal/', portal_view, name='portal'),
+    path('portal/', portal_view, name='portal'),
     path('confirmation/<int:session_id>/', confirmation_view, name='confirmation'),
 ]
 
